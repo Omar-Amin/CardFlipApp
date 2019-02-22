aaf21fd5c0ca2df23a7f81ac1dfdd7d34b2d9775
Omar Amin <omar_75@hotmail.dk>
2019-02-22 20:00:08 +0100

    Added scoreboard and reset button

--- app/src/main/java/com/example/cardflip/Deck.java
+++ app/src/main/java/com/example/cardflip/Deck.java
@@ -1,28 +1,31 @@
 package com.example.cardflip;
 
+import android.graphics.Color;
+import android.util.Pair;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 class Deck {
 
-
     Deck(){
     }
 
-    ArrayList<Integer> makeDeck(){
-        ArrayList<Integer> deck = new ArrayList<>();
+    ArrayList<ColorData> makeDeck(){
+        ArrayList<ColorData> deck = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
-            deck.add(R.drawable.orangec);
-            deck.add(R.drawable.bluec);
-            deck.add(R.drawable.purplec);
-            deck.add(R.drawable.yellowc);
-            deck.add(R.drawable.greenc);
+            deck.add(new ColorData(R.drawable.orangec, Color.parseColor("#EF7C2E")));
+            deck.add(new ColorData(R.drawable.bluec,Color.parseColor("#62A8DD")));
+            deck.add(new ColorData(R.drawable.purplec,Color.parseColor("#A0599F")));
+            deck.add(new ColorData(R.drawable.yellowc,Color.parseColor("#EEE75C")));
+            deck.add(new ColorData(R.drawable.greenc,Color.parseColor("#71B62E")));
         }
 
         Collections.shuffle(deck);
 
         return deck;
     }
+
 
 }
--- app/src/main/java/com/example/cardflip/MainActivity.java
+++ app/src/main/java/com/example/cardflip/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.cardflip;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -12,8 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11;
     ImageView[] visualCards = {card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11};
-    ArrayList<Integer> cards;
+    ArrayList<ColorData> cards;
     ArrayList<String> rFlip;
+    TextView scoreBoard;
+    TextView gameStatusTV;
+    String gameStatusStr;
+    Button tryAgain;
+
+    int scoreAdd = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +30,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        Deck deck = new Deck();
-        RandomFlip randomFlip = new RandomFlip(11);
-
-        rFlip = randomFlip.randomize();
-
-        cards = deck.makeDeck();
+        gameStatusTV = (TextView) findViewById(R.id.gameStatus);
+        tryAgain = (Button) findViewById(R.id.finishButton);
+        scoreBoard = (TextView) findViewById(R.id.score);
 
         card1 = (ImageView) findViewById(R.id.card1);
         card2 = (ImageView) findViewById(R.id.card2);
@@ -52,9 +58,27 @@ public class MainActivity extends AppCompatActivity {
         visualCards[9] = card10;
         visualCards[10] = card11;
 
+        makeGame();
+
+    }
+
+
+    private void makeGame(){
+        tryAgain.setOnClickListener(null);
+        tryAgain.setVisibility(View.INVISIBLE);
+        gameStatusTV.setVisibility(View.INVISIBLE);
+
+        Deck deck = new Deck();
+        RandomFlip randomFlip = new RandomFlip(11);
+
+        rFlip = randomFlip.randomize();
+
+        cards = deck.makeDeck();
+
+
         for (int i = 0; i < visualCards.length; i++) {
             if (rFlip.get(i+1).equals("1")) {
-                visualCards[i].setImageResource(cards.get(i));
+                visualCards[i].setImageResource(cards.get(i).getDeckVal());
 
                 visualCards[i].setOnClickListener(new View.OnClickListener() {
                     public void onClick(View view) {
@@ -63,17 +87,22 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }
-
     }
 
     private void changeCard(View view){
         ImageView imageView = (ImageView) view;
+        scoreAdd += 5;
 
         for (int i = 0; i < visualCards.length; i++) {
+            //searching for the item
             if(visualCards[i].getId() == view.getId()){
+                scoreBoard.setTextColor(cards.get(i).getColors());
+                scoreBoard.setText(scoreAdd+"");
                 rFlip.set(i+1,"R"); //update it as removed
-                if(rFlip.get(i).equals("0") && i != 0){ //add eventlistener and flip the card, update rFlip
-                    visualCards[i-1].setImageResource(cards.get(i-1));
+
+                //add/remove eventlistener and flip the left card, update rFlip
+                if(rFlip.get(i).equals("0") && i != 0){
+                    visualCards[i-1].setImageResource(cards.get(i-1).getDeckVal());
                     visualCards[i-1].setOnClickListener(new View.OnClickListener() {
                         public void onClick(View view) {
                             changeCard(view);
@@ -85,8 +114,10 @@ public class MainActivity extends AppCompatActivity {
                     visualCards[i-1].setOnClickListener(null);
                     rFlip.set(i,"0");
                 }
+
+                //add/remove eventlistener and flip the right card, update rFlip
                 if (rFlip.get(i+2).equals("0") && i != visualCards.length-1){
-                    visualCards[i+1].setImageResource(cards.get(i+1));
+                    visualCards[i+1].setImageResource(cards.get(i+1).getDeckVal());
                     visualCards[i+1].setOnClickListener(new View.OnClickListener() {
                         public void onClick(View view) {
                             changeCard(view);
@@ -100,8 +131,55 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+        gameStatusStr = checkGame();
 
-        imageView.setImageBitmap(null);
+        imageView.setVisibility(View.INVISIBLE);
         imageView.setOnClickListener(null);
     }
+
+    String checkGame(){
+        for (int i = 1; i < rFlip.size()-1; i++) {
+            if (rFlip.get(i).equals("1")){
+                return "still";
+            }
+        }
+
+        tryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetGame();
+                makeGame();
+            }
+        });
+
+        for (int i = 1; i < rFlip.size()-1; i++) {
+            if (rFlip.get(i).equals("0")){
+                tryAgain.setTextColor(Color.RED);
+                gameStatusTV.setText("You lost...");
+                gameStatusTV.setTextColor(Color.RED);
+                gameStatusTV.setVisibility(View.VISIBLE);
+                tryAgain.setVisibility(View.VISIBLE);
+                return "lost";
+            }
+        }
+        tryAgain.setTextColor(Color.parseColor("#2d9604"));
+        gameStatusTV.setText("You won!!!");
+        gameStatusTV.setTextColor(Color.parseColor("#2d9604"));
+        gameStatusTV.setVisibility(View.VISIBLE);
+        tryAgain.setVisibility(View.VISIBLE);
+        return "won";
+
+    }
+
+    private void resetGame(){
+
+        for (ImageView visualCard : visualCards) {
+            visualCard.setVisibility(View.VISIBLE);
+            visualCard.setImageResource(R.drawable.greyc2);
+        }
+        scoreBoard.setText("0");
+        scoreAdd = 0;
+
+    }
+
 }
